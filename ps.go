eb6f7141549c40e718d1bebaa070bfed03267091
Alireza <26859725+tralireza@users.noreply.github.com>
2022-10-03 19:02:00 +0100

    Prefix Sum 523m Coninuous Subarray Sum -> modular equality
    Go

--- ps.go
+++ ps.go
@@ -1,7 +1,37 @@
 package PrefixSum
 
-import "log"
+import (
+	"log"
+)
 
 func init() {
 	log.SetFlags(0)
+}
+
+// 523m Continuous Subarray Sum
+func checkSubarraySum(nums []int, k int) bool {
+	/*
+	   Prefix_i = a_0 + ... + a_i
+	   Sum(Subarray[i+1, j]) = Prefix_j - Prefix_i
+
+	   Prefix_j - Prefix_i % k == 0  =>  Prefix_j == Prefix_i (mod k)
+	*/
+
+	Index := map[int]int{0: -1}
+
+	mpfx := 0
+	for r := range nums {
+		mpfx += nums[r]
+		mpfx %= k
+
+		if l, ok := Index[mpfx]; ok {
+			if l+1 < r {
+				return true
+			}
+		} else {
+			Index[mpfx] = r
+		}
+	}
+
+	return false
 }
--- ps_test.go
+++ ps_test.go
@@ -1,7 +1,17 @@
 package PrefixSum
 
-import "log"
+import (
+	"log"
+	"testing"
+)
 
 func init() {
-	log.Print("> PrefixSum")
+	log.Print("> Prefix Sum")
+}
+
+// 523m Continuous Subarray Sum
+func Test523(t *testing.T) {
+	log.Print("true ? ", checkSubarraySum([]int{23, 2, 4, 6, 7}, 6))
+	log.Print("true ? ", checkSubarraySum([]int{23, 2, 6, 4, 7}, 6))
+	log.Print("false ? ", checkSubarraySum([]int{23, 2, 6, 4, 7}, 13))
 }
